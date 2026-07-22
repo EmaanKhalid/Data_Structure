2053d8357991580630844f114ecb89c107658925
Administrator <emaankhalid858@gmail.com>
2026-07-22 15:31:15 +0500

    Add new file

---
+++ updatingList.py
@@ -0,0 +1,13 @@
+lst_2 = list()
+for i in range(1,6):
+    lst_2.append(i)
+
+print(lst_2)
+
+#updating the value at a specific index
+lst_2[1] = 'a'
+print('lst_2 after updating using index:',lst_2)
+
+#updating the value using slicing
+lst_2[2:4] = [2,'f',5.5]
+print('lst_2 after updating with slicing:',lst_2)
